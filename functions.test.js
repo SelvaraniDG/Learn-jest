62ad8251a71c2cb8c71dbb410a3878bff3995d47
Selvarani CR <Selvarani.cr@datagrokr.com>
2024-04-03 10:42:12 +0530

    Learnt the basics

---
+++ functions.test.js
@@ -0,0 +1,45 @@
+const functions = require('./functions');
+//toBe
+test('Adds 2 + 2 to equal 4', () => {
+    expect(functions.add(2,2)).toBe(4);
+});
+
+//not.toBe
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2,2)).not.toBe(5);
+});
+
+//toBeNull
+test('Should be a null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+//toBeFalsy
+test('Should be falsy', () => {
+    expect(functions.checkVlaue()).toBeFalsy();
+});
+
+//.toStrictEqual or .toEqual
+test('Should be Brad Traversy object', () => {
+    expect(functions.createUser()).toStrictEqual({
+        firstName: 'Brad',
+        lastName: 'Traversy'
+    });
+});
+
+//less than and greater than
+test('should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+})
+
+//Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+})
+//toContain
+test('Admin should be in usernames', () => {
+    usernames = ['john', 'brad', 'admin'];
+    expect(usernames).toContain('admin');
+})
